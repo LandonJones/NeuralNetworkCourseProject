52bc9656fc0d5fed8d8583f164363b64e69d24ea
Affan <affandhankwala@gmail.com>
2024-12-07 15:53:18 -0700

    simple_perceptron

--- Aurora_PA7/FFBP.py
+++ Aurora_PA7/FFBP.py
@@ -1,4 +1,4 @@
-
+from typing import List
 
 def error(output, desired_output):
     return desired_output - output
@@ -45,6 +45,49 @@ class Network:
 # Formula of compute E:
 # E = 0.5 * error ** 2
 
+class Simple_Perceptron:
+    def __init__(self, weights: List, eta: float):
+        self.weights = weights
+        self.bias = 0
+        self.eta = eta
+
+    def calculate(self, inputs: List) -> float:
+        return self.weights[0] * inputs[0] + self.weights[1] * inputs[1] + self.bias
+    
+    def error_correct(self, inputs: List, predicted: float, expected: float):
+        # Correct the weights and biases based on performance
+        # Weight update formula = wi_original + eta * (y_true - y_pred) * x_i
+        for i in range(len(self.weights)):
+            self.weights[i] += self.eta * (expected - predicted) * inputs[i]
+        # Update bias
+        self.bias += self.eta * (expected - predicted)
+
+    def round_predicted(self, predicted: float) -> int: 
+        # Round the predicted value to either 0 or 1
+        if predicted > 0.5: 
+            return 1
+        else: 
+            return 0
+
+    def train(self, inputs: List, expected: List, iterations: int):
+        # Iterate over certain iterations
+        for i in range(iterations):
+            # Iterate over all inputs
+            for data in range(len(inputs)):
+                # Present i/o pair and calculate expected with rounding
+                predicted = self.round_predicted(self.calculate(inputs[data]))
+                # Update weights
+                self.error_correct(inputs[data], predicted, expected[data])
+        return 0
+            
+    def test(self, inputs: List) -> List: 
+        # Iterate over all inputs
+        predicted_list = []
+        for data in range(len(inputs)):
+            # Present input pair to calculate predicted value
+            predicted_list.append(self.round_predicted(self.calculate(inputs[data])))
+        # Return the predicted values
+        return predicted_list
 
 
 
